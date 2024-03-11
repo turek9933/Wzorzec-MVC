3eec9902cc0836e07fe97ea6aa24d7a4b0dd3fce
Tomasz <103149994+turek9933@users.noreply.github.com>
2024-03-11 23:18:33 +0100

    Final student.js
    Usunięcie zbędnego kodu

--- zadanie 1/student.js
+++ zadanie 1/student.js
@@ -1,6 +1,4 @@
 let FULLNAME = 'Tomasz Turek';
 let STUDENT_ID = 44674;
-let message = "My name is ${asdsa}. My student ID is ${STUDENT_ID}";
-
 
 console.log("My name is " + FULLNAME + ". My student ID is " + STUDENT_ID + ".");
