82ad25436482e66470bf28731e201c882da05fbd
Tomasz <103149994+turek9933@users.noreply.github.com>
2024-03-11 23:46:18 +0100

    Update app.js

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ function requestListener(request, response) {
     response.setHeader("Content-Type", "text/html");
     response.write("<html>");
     response.write("<head><title>About student</title></head>");
-    response.write("<body><p>" + "My name is " + student.getStudentFullName() + ". My student ID is " + student.getStudentId() + "." + "</p></body>");
+    response.write("<body><p>" + student.printStudent() + "</p></body>");
     response.write("</html>");
     response.end();
 }
