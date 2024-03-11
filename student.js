e091f26faf34fed6a56a074ad6f731ca9888eb8a
Tomasz <103149994+turek9933@users.noreply.github.com>
2024-03-11 23:24:40 +0100

    Update student.js
    Usunięcie zbędnego kodu

--- student.js
+++ student.js
@@ -1,3 +1,18 @@
 let FULLNAME = 'Tomasz Turek';
 let STUDENT_ID = 44674;
-console.log("My name is " + FULLNAME + ". My student ID is " + STUDENT_ID + ".");
+
+function getStudentFullName() {}
+function getStudentId() {}
+function printStudent() {}
+
+function getStudentFullName() {
+    return FULLNAME;
+}
+function getStudentId() {
+    return STUDENT_ID;
+}
+function printStudent() {
+    console.log("My name is " + getStudentFullName() + ". My student ID is " + getStudentId() + ".");
+}
+
+printStudent();
