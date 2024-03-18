611f6aa5c8056f79724fd930515cf728ab889ace
Tomasz <103149994+turek9933@users.noreply.github.com>
2024-03-18 19:55:08 +0100

    Update student.js

--- views/student.js
+++ views/student.js
@@ -1,18 +1,27 @@
-function renderPage(response) {
-    response.setHeader("Content-Type", "text/html");
+const fs = require("fs");
+
+function renderPage(response, data) {
     response.write(`
-    <html lang=”pl”>
-        <head>
-            <meta charset="UTF-8">
-            <meta name="viewport" content="width=device-width, initial-scale=1.0">
-            <title>Student</title>
-        </head>
-        <body>
-            <div>Student wita home</div>
-        </body>
-    </html>
+        <html lang="pl">
+            <head>
+                <meta charset="UTF-8">
+                <meta name="viewport" content="width=device-width, initial-scale=1.0">
+                <title>Student</title>
+            </head>
+            <body>
+                <h1>Student profile</h1>
+                <div>
+                    <p>Imię: ${data.name}</p>
+                    <p>Nazwisko: ${data.lastname}</p>
+                    <p>Wiek: ${data.age}</p>
+                    <p>Płeć: ${data.gender}</p>
+                    <p>Numer albumu: ${data.code}</p>
+                    <p>Kierunek: ${data.studyField}</p>
+                </div>
+            </body>
+        </html>
     `);
-    return response.end()
+    response.end();
 }
 module.exports = {
     renderPage,
