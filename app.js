dd89b21cc615a8d9759a295aef9899616059ecdc
Tomasz <103149994+turek9933@users.noreply.github.com>
2024-03-18 19:44:31 +0100

    Update app.js

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const student = require("./views/student.js");
 const PORT = 3000;
 
 function requestListener(request, response) {
-    const {url, method} = request;
+    const { url, method } = request;
     console.log(`Server is running on ${PORT}`);
     if (url === "/") {
         response.setHeader("Content-Type", "text/html");
@@ -16,20 +16,45 @@ function requestListener(request, response) {
             <form action="/home" method="">
                 <button>home</button>
             </form>
-            <form action="/student" method="">
+            <form action="/student" method="POST">
                 <button>Student</button>
             </form>
         </body>
         `);
         response.write("</html>");
         return response.end();
+    } else if (url === "/student" && method === "POST") {
+        let body = "";
+        request.on("data", (chunk) => {
+            body += chunk.toString(); 
+        });
+        request.on("end", () => {
+            response.writeHead(302, { Location: "/student" });
+            response.end();
+        });
     }
-    if (url === "/student") {
+    else if (url === "/student") {
         student.renderPage(response);
     }
-    //if (url === "/home") {
-    else {
+    else if (url === "/home") {
         home.renderPage(response);
+    }
+    
+    else {
+        response.write("<html>");
+        response.write("<head><title>Blad</title></head>");
+        response.write(`
+        <body>
+            404 Not Found
+        </body>
+        `);
+        response.write("</html>");
+        return request.on("end", () => {
+            response.statusCode = 404;
+            response.setHeader("Location", "/");
+            response.setHeader("Content-Type", "text/html");
+            response.end();
+        })
     }
 }
 
