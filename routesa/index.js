e6f995e226859da6d5f930709cdd85101e2e37d6
Tomasz <103149994+turek9933@users.noreply.github.com>
2024-03-18 19:39:35 +0100

    Update index.js

--- routesa/index.js
+++ routesa/index.js
@@ -1,1 +1,10 @@
-dasd
+const http = require("http");
+const home = require("./views/home.js");
+const student = require("./views/student.js");
+
+function handleHome(response) {
+    home.renderPage(response);
+}
+function handleStudent(response) {
+    student.renderPage(response);
+}
