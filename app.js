2ff23c0ff425ed815c3db4b36948d60a30c35ced
Tomasz Turek <turek9933@gmail.com>
2024-03-24 15:38:25 +0100

    Nauka - po terminie

--- app.js
+++ app.js
@@ -4,35 +4,20 @@ const student = require("./views/student.js");
 
 const PORT = 3000;
 
-function requestListener(request, response) {
-    const {url, method} = request;
-    console.log(`Server is running on ${PORT}`);
+function requestListener(req, res) {
+    const {url} = req;
     if (url === "/") {
-        response.setHeader("Content-Type", "text/html");
-        response.write("<html>");
-        response.write("<head><title>My second Node App</title></head>");
-        response.write(`
-        <body>
-            <form action="/home" method="">
-                <button>home</button>
-            </form>
-            <form action="/student" method="">
-                <button>Student</button>
-            </form>
-        </body>
-        `);
-        response.write("</html>");
-        return response.end();
+        home.renderPage(res);
     }
     else if (url === "/student") {
-        student.renderPage(response);
+        student.renderPage(res);
     }
-    //if (url === "/home") {
     else {
-        home.renderPage(response);
+        res.writeHead(404, {'Content-Type': 'text/html'});
+        res.end('<h1>404 Not Found</h1>');
     }
 }
 
 const server = http.createServer(requestListener);
 
-server.listen(PORT);
+server.listen(PORT, console.log(`Server is running on ${PORT}`));
