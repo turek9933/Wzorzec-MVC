a98f4bcdaddb0a4f07f0bad0a94d1bb8eb87eb2a
Tomasz Turek <turek9933@gmail.com>
2024-03-25 20:10:51 +0100

    Zadanie 4, nie dokończone

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const express = require('express');
 const app = express();
 const PORT = 3000;
 
+app.use(express.static('public'));
+
 app.use((req, res, next) => {
   const method = req.method;
   const url = req.url;
@@ -15,16 +17,7 @@ app.use(express.urlencoded({extended: true}));
 let students = [];
 
 app.get('/home', (req, res) => {
-  res.send(`
-  <html>
-  <head>
-    <title>Home</title>
-  </head>
-  <body>
-    <p>HOME</p>
-  </body>
-  </html>`
-  );
+  res.sendFile('views/home.html', {root: __dirname});
 });
 
 app.post('/student', (req, res) => {
@@ -37,29 +30,17 @@ app.post('/student', (req, res) => {
     <body><p>Hello, ${firstName} ${lastName} on ${fieldOfStudy} studies!</p></body>
     </html>
     `);
-  });
+});
 
 app.get('/add-student', (req, res) => {
-    res.send(`
-    <html>
-    <head><title>Add Student</title></head>
-    <body>
-    <form action="/student" method="post">
-        <input type="text" name="firstName" />
-        <input type="text" name="lastName" />
-        <input type="text" name="fieldOfStudy" />
-        <button type="submit">Wyślij</button>
-    </form>
-    </body>
-    </html>
-    `);
+  res.sendFile('views/add-student.html', {root: __dirname});
 });
 
 app.get('/users', (req, res) => {
     const studentListItems = students.map(student => 
       `<li><p>${student.firstName} ${student.lastName} - ${student.fieldOfStudy}</p></li>`
     ).join('');
-  
+
     res.send(`
     <html>
     <head><title>Users List</title></head>
@@ -68,7 +49,6 @@ app.get('/users', (req, res) => {
     </body>
     </html>`);
 });
-
 
 app.listen(PORT, () => {
   console.log(`Server is running on PORT:${PORT}`);
