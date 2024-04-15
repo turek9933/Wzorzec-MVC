baa8a7fa362f19774fe4872f5597f93adf973a86
Tomasz Turek <turek9933@gmail.com>
2024-04-15 19:49:00 +0200

    Zadanie 3, Szybko, puki czas

--- controllers/books.js
+++ controllers/books.js
@@ -3,12 +3,31 @@ const Book = require('../models/book');
 let books = [];
 let freeId = 0;
 
-books.push(new Book(freeId++, 'O obrotach cial zielonych', 1969, 1));
-
 const getBooksView = (req, res) => {
     res.render('books', {pageTitle: 'Books List', books: books});
 };
 
+const getAddBookView = (req, res) => {
+    res.render('add-book', {pageTitle: 'Add Book'});
+};
+
+const postAddBook = (req, res) => {
+    const {title, publishingYear, authorId} = req.body;
+    const bookToPush = new Book(freeId++, title, publishingYear, authorId);
+    books.push(bookToPush);
+    res.redirect('/book/list');
+};
+
+const postDeleteBook = (req, res) => {
+    const {id} = req.params;
+    const fixedId = parseInt(id);
+    books = books.filter((book) => book.id !== fixedId);
+    res.redirect('/book/list');
+}
+
 module.exports = {
     getBooksView,
+    getAddBookView,
+    postAddBook,
+    postDeleteBook,
 }
--- routes/book.js
+++ routes/book.js
@@ -4,4 +4,10 @@ const booksController = require('../controllers/books');
 
 router.get('/book/list', booksController.getBooksView);
 
+router.get('/book/add', booksController.getAddBookView);
+
+router.post('/book/add', booksController.postAddBook);
+
+router.post('/book/delete/:id', booksController.postDeleteBook);
+
 module.exports = router;
